ea220fe5fd9cfb77e0aa0ffee0919d0c253c7cf3
SajBajra <sajesh.bajracharya.01@gmail.com>
2026-02-16 18:45:46 +0545

    everything pushed

---
+++ wp-content/uploads/smush/resize-log.php
@@ -0,0 +1,6 @@
+<?php die(); ?>
+[2026-02-12T14:19:53+00:00] Notice: File saved successfully in 0.03 seconds
+[2026-02-12T14:20:35+00:00] Notice: File saved successfully in 0.03 seconds
+[2026-02-12T14:21:23+00:00] Notice: File saved successfully in 0.04 seconds
+[2026-02-12T14:22:00+00:00] Notice: File saved successfully in 0.04 seconds
+[2026-02-12T14:22:04+00:00] Notice: File saved successfully in 0.03 seconds
