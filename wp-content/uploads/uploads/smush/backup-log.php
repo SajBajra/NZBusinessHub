e611b6bb271d41e33430ce77555a4c6e94459273
SajBajra <sajesh.bajracharya.01@gmail.com>
2026-03-02 18:21:13 +0545

    Add media uploads and backup configs

---
+++ wp-content/uploads/uploads/smush/backup-log.php
@@ -0,0 +1,6 @@
+<?php die(); ?>
+[2026-02-12T14:19:48+00:00] Error: Smush already in progress
+[2026-02-12T14:20:33+00:00] Error: Smush already in progress
+[2026-02-12T14:21:20+00:00] Error: Smush already in progress
+[2026-02-12T14:21:57+00:00] Error: Smush already in progress
+[2026-02-12T14:22:02+00:00] Error: Smush already in progress
